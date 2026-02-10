4bfbbc5498e09caab4d568fd8d97d3b242d505fe
Magali Tamayo Carrillo <86676241+MagaliTC07@users.noreply.github.com>
2026-02-10 11:14:32 -0600

    Update index.ts

--- supabase/functions/people/index.ts
+++ supabase/functions/people/index.ts
@@ -9,8 +9,9 @@ const corsHeaders = {
 //const MONGODB_URI = Deno.env.get("MONGODB_URI");
 //const MONGODB_DATABASE = Deno.env.get("MONGODB_DATABASE");
 //const MONGODB_URI = 'mongodb+srv://fox1:modelotx30@arcobitscluster0.w6meunj.mongodb.net/?retryWrites=true&w=majority&appName=ArcobitsCluster0';
-const MONGODB_URI = 'mongodb+srv://singularityatlas:yHAuUQlpYrD16JkL@cluster0.m2t3c.mongodb.net/';
+const MONGODB_URI = 'mongodb+srv://fox1:modelotx30@arcobitscluster0.w6meunj.mongodb.net/?retryWrites=true&w=majority&appName=ArcobitsCluster0';
 const MONGODB_DATABASE = 'singulatiry_sandbox';
+
 
 let cachedClient: MongoClient | null = null;
 
