a8fd266447bbfb86573e2c3e4a2841de5e7b9937
EObarrientosM <erick.barrientos@kromlogistica.com>
2026-05-15 10:51:10 -0600

    Update dashboardService
    se apunta al env para produccion

--- src/services/dashboardService.ts
+++ src/services/dashboardService.ts
@@ -97,7 +97,7 @@ export async function fetchDashboardStats(): Promise<DashboardStats> {
   return await response.json();
 }
 
-const DASHBOARD_API_URL = import.meta.env.VITE_API_DASHBOARD;
+const DASHBOARD_API_URL = import.meta.env.VITE_API_URL;
 const API_TOKENSL = import.meta.env.VITE_TOKENSL;
 const API_KEYSL = import.meta.env.VITE_APIKEYSL;
 
